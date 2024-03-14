3f9399233ead8b766aaa70bd36f139afda268a64
Moses-Aduojo <temojokego@gmail.com>
2024-03-14 12:19:30 -0700

    redo

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-
-_add = add(a, b)
-
 if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+
+    _add = add(a, b)
     print(f"{a} + {b} = {_add}")
