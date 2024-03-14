a2ce63f7269ebb3a3d6788c0984e7dee437e35f0
Moses-Aduojo <temojokego@gmail.com>
2024-03-14 09:17:55 -0700

    editted

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
+from add_0 import add
 a, b = 1, 2
 
 if __name__ == "__main__":
-    from add_0 import add
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print(f"{a} + {b} = {add(a, b)}")
