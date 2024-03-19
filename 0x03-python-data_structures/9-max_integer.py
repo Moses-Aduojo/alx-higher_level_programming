64f5c4ebae85d79560a4d91505ef1e0b2860d95e
Moses-Aduojo <temojokego@gmail.com>
2024-03-19 08:58:53 -0700

    find max value of a list items

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max_val = my_list[0]
+    i = 1
+    while i < len(my_list):
+        if my_list[i] > max_val:
+            max_val = my_list[i]
+        i += 1
+    return max_val
