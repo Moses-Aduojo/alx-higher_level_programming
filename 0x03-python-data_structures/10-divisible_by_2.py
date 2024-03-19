f12fcf3829d5165fbefe32b38b0e1ebc3e4c2e14
Moses-Aduojo <temojokego@gmail.com>
2024-03-19 07:52:50 -0700

    re-editted

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    if my_list == None:
+    if my_list is None:
         return
     else:
-        new_list = my_list[:]
-        for i in range(len(new_list) - 1):
-            if new_list[i] < 0:
-                new_list[i] = new_list[i] * -1
-            if new_list[i] % 2 == 0:
-                new_list[i] = True
+        new_list = []
+        for item in my_list:
+            if item % 2 == 0:
+                new_list.append(True)
             else:
-                new_list[i] = False
+                new_list.append(False)
         return new_list
