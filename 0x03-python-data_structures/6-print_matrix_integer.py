929369e89314e75881598d0ca9dccb1235127e0e
Moses-Aduojo <temojokego@gmail.com>
2024-03-19 09:45:30 -0700

    enumerate is powerful

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-    for arr in matrix:
-        for item in arr:
+    for row in matrix:
+        for i, item in enumerate(row):
+            if i != 0:
+                print(" ", end="")
             print("{:d}".format(item), end="")
         print()
