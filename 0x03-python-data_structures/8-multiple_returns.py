205f69c79f9c4e0c2e7c117e41b495efb3b9963e
Moses-Aduojo <temojokego@gmail.com>
2024-03-19 08:30:47 -0700

    multiple_ return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,8 +3,7 @@
 def multiple_returns(sentence):
     if sentence is None:
         a = None
-        str_len = 0
     else:
         a = sentence[0]
-        str_len = len(sentence)
+    str_len = len(sentence)
     return (str_len, a)
