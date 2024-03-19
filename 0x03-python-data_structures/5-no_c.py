278bad3e6d7e85519a97e280147f35403baf964f
Moses-Aduojo <temojokego@gmail.com>
2024-03-19 07:10:50 -0700

    editted

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
+    if my_string is None:
+        return
     new_str = ""
     for i in range(len(my_string) - 1):
         if my_string[i] != 'c' and my_string[i] != 'C':
