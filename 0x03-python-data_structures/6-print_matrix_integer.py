3adbf80a9f602ab2d2e1becfb5854b33cc4b2a61
Moses-Aduojo <temojokego@gmail.com>
2024-03-19 15:37:58 -0700

    delete a list item using del

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, item in enumerate(row):
+            #  for a given iteration enumerate returns a tuple\
+            #  of each item and index
             if i != 0:
                 print(" ", end="")
             print("{:d}".format(item), end="")
