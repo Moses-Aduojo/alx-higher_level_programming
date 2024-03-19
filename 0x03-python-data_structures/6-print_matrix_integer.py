3b5530aed22a8ed1856e50d5ccf7e04adb45b9e6
Moses-Aduojo <temojokego@gmail.com>
2024-03-19 09:35:29 -0700

    print element of a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return
     for arr in matrix:
         for item in arr:
             print("{:d}".format(item), end="")
