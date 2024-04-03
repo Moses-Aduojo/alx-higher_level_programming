1283ae5e2a5a5e39f50a03d7bd1d1a17cb53f80a
Moses-Aduojo <temojokego@gmail.com>
2024-04-03 07:05:00 -0700

    error to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except (TypeError, ValueError) as e:
+    except ValueError as e:
         print("Exception: ", e, file=sys.stderr)
         return False
     return True
