a13084b93059744ff9ea0dedebcfab96e6e26ce0
Moses-Aduojo <temojokego@gmail.com>
2024-04-04 08:22:24 -0700

    Public instance method: def area(self): that returns the current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+
+"""
+This module provides functions and class for geometric shape.
+
+Contents:
+- Square: a square class.
+"""
+
+
+class Square:
+    """
+    Represents a square geometric shape.
+
+    Attributes:
+        __size (int): The size of each side of the square.
+
+    Methods:
+        None
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new Square object with the specified size.
+
+        Parameters:
+            size (int): The length of each side of the square.
+        Raises:
+            ValueError: If size is less than 0.
+            TypeError: If size is not an integer.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        compute the area of the square object
+
+        parameters:
+            size (int): gives the dimension of the square
+        Return: the area of the square object
+        """
+        return self.__size ** 2
