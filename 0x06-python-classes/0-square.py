fbba7fe5125dfd162469fd7b2e50c41467d0ee05
Moses-Aduojo <temojokego@gmail.com>
2024-04-03 20:01:52 -0700

    doc string added

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+"""
+This module provides functions and class for geometric shape.
+
+Contents:
+- Square: a square class.
+"""
 class Square:
     """
     Represents a square geometric shape.
