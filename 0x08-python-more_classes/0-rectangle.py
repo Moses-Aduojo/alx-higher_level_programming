66c08bf949c6979ae542c922610d6436d4467b8c
Moses-Aduojo <temojokego@gmail.com>
2024-04-08 08:18:19 -0700

    empty class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This modules defines a class rectangle:
+it's attribute and properties are encapsulated
+"""
+
+
+class Rectangle:
+    """
+    This class provide a a template for the creation of
+    a Rectangle object
+    """
+    def __init__(self):
+        """
+        intitialized Rectangle object properties when a new object is
+        created
+        """
+        pass
