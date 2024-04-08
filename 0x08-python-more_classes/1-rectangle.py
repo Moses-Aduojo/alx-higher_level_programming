7235e7c198b61f900f673e77f07cff1814afaf14
Moses-Aduojo <temojokego@gmail.com>
2024-04-08 15:22:36 -0700

    setting and getting private attribute using property

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,8 +17,8 @@ class Rectangle:
         created
         """
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -28,17 +28,17 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def width(self, width):
+    def width(self, value):
         """
         set the width attribute conditionally
         """
 
-        if not isinstance(width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif self.width < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         else:
-            self.__width = width
+            self.__width = value
 
     @property
     def height(self):
@@ -49,14 +49,14 @@ class Rectangle:
         return self.__height
 
     @height.setter
-    def height(self, height):
+    def height(self, value):
         """
         set the height attribute conditionally
         """
 
-        if not isinstance(height, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif height < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         else:
-            self.__height = height
+            self.__height = value
