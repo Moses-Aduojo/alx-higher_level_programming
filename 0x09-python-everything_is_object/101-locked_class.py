d596a5a993df18baedfe3dd6be6e1eb5c4671d12
Moses-Aduojo <temojokego@gmail.com>
2024-04-10 22:12:20 -0700

    python3 chaching

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,6 +5,7 @@ prevents the user from dynamically creating new instance attributes,\
 except if the new instance attribute is called first_name.
 """
 
+
 class LockedClass:
     """A class with restricted attribute creation.
 
@@ -16,4 +17,3 @@ class LockedClass:
 
     def __init__(self):
         pass
-
