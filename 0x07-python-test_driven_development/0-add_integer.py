bfc2bb976600a9b22071cd75d781cd0ed9e5de74
Moses-Aduojo <temojokego@gmail.com>
2024-04-17 05:56:32 -0700

    edit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,11 +16,17 @@ def add_integer(a, b=98):
 
     Return: the  sum of a and b in integer
     """
+    if a == 'NaN':
+        a = float(a)
+    if b == 'NaN':
+        b == float(b)
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
 
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+    if a != a or b != b:
+        raise ValueError('cannot convert float NaN to integer')
 
     a = int(a)
     b = int(b)
