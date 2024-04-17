80e73aa39c63c4ebb0f65fd505f3f40be146fbe8
Moses-Aduojo <temojokego@gmail.com>
2024-04-17 08:10:20 -0700

    printing attribute and method of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+workning with object
+"""
+
+def lookup(obj):
+    """
+    return the list of attributes and properies of an object
+    """
+
+    mylist = dir(obj)
+    return mylist
