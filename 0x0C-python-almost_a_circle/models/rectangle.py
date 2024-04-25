d9898640f1c86b6f68cd304af318aaa28700a563
Moses-Aduojo <temojokego@gmail.com>
2024-04-25 05:14:29 -0700

    new

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -127,7 +127,7 @@ class Rectangle(Base):
         """
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("x must be > 0")
         self.__x = value
 
@@ -151,6 +151,6 @@ class Rectangle(Base):
         """
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("y must be > 0")
         self.__y = value
