8e07afafe91f3de884ecd198bd4823fabacfd63e
Moses-Aduojo <temojokego@gmail.com>
2024-04-27 05:59:09 -0700

    dictrepresentation to json saved to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,7 +29,29 @@ class Base:
         """return json list representaion of list_dictionary
         parameter: list_dictionaries - a list of dictionary
         """
+        print(type(list_dictionaries))
+        print(list_dictionaries)
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file."""
+        if list_objs is None:
+            list_objs = []
+
+        """construct file name"""
+        class_name = cls.__name__
+        filename = "{}.json".format(class_name)
+
+        """get dictionary representation of object"""
+        dict_list = [(obj.to_dictionary()) for obj in list_objs]
+
+        """convert to json string"""
+        json_string = cls.to_json_string(dict_list)
+
+        """save to file named filename"""
+        with open(filename, 'w') as file:
+            file.write(json_string)
